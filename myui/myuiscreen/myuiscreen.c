3dd50f4ab0276d2bac2916bd0837704d9c849360
Jing Lin <Jinglin82@gmail.com>
2013-12-10 10:06:26 -0500

    "almost done add"

--- myui/myuiscreen/myuiscreen.c
+++ myui/myuiscreen/myuiscreen.c
@@ -102,6 +102,47 @@ void LifeTracker() {
   xt_par0(XT_CH_WHITE);
 }
 
+void addscreen(){
+  xt_par0(XT_CLEAR_SCREEN);
+  xt_par2(XT_SET_ROW_COL_POS, row = 1, col = 1);
+  xt_par0(XT_CH_GREEN);
+  printf("----------------------------------------------------LifeTracker---------------------------------------------------------");
+  xt_par0(XT_CH_WHITE);
+  printf("                                  Number of Records:---| Author:------| Version:-.--\n                       First Record Time:------------:--:--| Last Record Time:------------:--:--\n------------------------------------------------------------------------------------------------------------------------");
+  xt_par0(XT_CH_BLUE);
+  printf("-------------------------------------UP/Down-Switch rows Left/Right-Switch columns--------------------------------------");
+  xt_par0(XT_CH_WHITE);
+  for(i = 0; i < 12; i++){
+    xt_par2(XT_SET_ROW_COL_POS, row = 11+i, col = 24);
+    printf("|");
+    xt_par2(XT_SET_ROW_COL_POS, row = 11+i, col = 96);
+    printf("|");
+  }
+  xt_par2(XT_SET_ROW_COL_POS, row = 11, col = 25);
+  printf("------------------------------Subject----------------------------------");
+  xt_par2(XT_SET_ROW_COL_POS, row = 16, col = 25);
+  printf("------------------------------");
+  xt_par2(XT_SET_ROW_COL_POS, row = 19, col = 25);
+  printf("----------------------------------------------------------------------");
+  xt_par2(XT_SET_ROW_COL_POS, row = 20, col = 25);
+  printf("----------------------------------------------------------------------");
+  xt_par0(XT_CH_GREEN);
+  xt_par2(XT_SET_ROW_COL_POS, row = 12, col = 25);
+  printf("Record - (--:--:--)");
+  xt_par2(XT_SET_ROW_COL_POS, row = 15, col = 25);
+  printf("Title:");
+  xt_par2(XT_SET_ROW_COL_POS, row = 18, col = 25);
+  printf("Body:");
+  xt_par0(XT_CH_BLUE);
+  xt_par2(XT_SET_ROW_COL_POS, row = 22, col = 25);
+  printf("------------------F2-Edit Title F3-Edit Body F7-Save-------------------");
+  xt_par2(XT_SET_ROW_COL_POS, row = 28, col = 1);
+  xt_par0(XT_CH_BLUE);
+  printf("-------------------------------------------------------F9-EXIT----------------------------------------------------------");
+  xt_par0(XT_CH_WHITE);
+}
+
+
 int main() {
   int c;
   int i, j, SUBJECT;
@@ -123,7 +164,7 @@ int main() {
     if(c == KEY_F9) break;
     else if(c == KEY_DOWN) {
       if(col == 2 && row >= 8 && row < 24) 
-          xt_par2(XT_SET_ROW_COL_POS,++row,col);
+          xt_par2(XT_SET_ROW_COL_POS,SUBJECT = ++row,col);
       if(col == 20 && row >= 7 && row <= 20)
           xt_par2(XT_SET_ROW_COL_POS,row+=5,col);
       if(col > 94 && col < 120){
@@ -137,7 +178,7 @@ int main() {
     }
     else if(c == KEY_UP){
       if(col == 2 && row > 8 && row <= 24)
-          xt_par2(XT_SET_ROW_COL_POS,--row,col);
+          xt_par2(XT_SET_ROW_COL_POS,SUBJECT = --row,col);
       if(col == 20 && row >= 8 && row < 24)
           xt_par2(XT_SET_ROW_COL_POS,row-=5,col);
       if(col > 94 && col < 120){
@@ -180,6 +221,10 @@ int main() {
       Board[row - 1][col - 1] = ' ';
       putchar(' ');
       xt_par2(XT_SET_ROW_COL_POS,row,col);
+    }
+    else if (c == KEY_F2){
+      addscreen();
+      xt_par2(XT_SET_ROW_COL_POS,row = 16, col = 25);
     }
     else if (c == KEY_F7){
       if(col < 95)
