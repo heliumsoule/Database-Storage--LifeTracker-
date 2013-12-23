6ec18f0760f5a34bb67bdbb1ce1d82e53185ae4d
Silvertongued <TheEighteenthBrumaire@Jings-MacBook-Pro.local>
2013-12-23 02:08:37 -0500

    I hate vim

--- myui/myui2FirstCut/myuiscreen.c
+++ myui/myui2FirstCut/myuiscreen.c
@@ -32,6 +32,9 @@ struct realCategory{
   char *category;
 };
 struct realCategory *categoryList;
+struct ArrayRecords *catStorage; 
+int catCounter;
+int tempCounter; //keeps track of max at total category
 int total;
 
 void parseInputForStat(){
@@ -177,9 +180,13 @@ int readmyStoreFromChild(char *argv1, char *argv2, char *argv3, char *argv4, cha
 
 void fillCategory(){
   categoryList = (struct realCategory*)malloc((maxRecord) * sizeof(struct realCategory));
+  catCounter = 0;
+  //categoryList[0].category = dataStorage[0].category;
+  //categoryList[1].category = dataStorage[1].category;
+  //catCounter = 2;
   int temp = 0;
   for(i = 0; i < maxRecord; i++){                                           //checks the record you are up to
-    for(j = 0; j < i; j++){                                                 //compares its category with each category in the list
+    for(j = 0; j < catCounter; j++){                                        //compares its category with each category in the list
       if(strcmp(dataStorage[i].category, categoryList[j].category) == 0){   //if the current record has a category that already exists,
 	temp = 1;                                                          
 	break;
@@ -187,10 +194,58 @@ void fillCategory(){
     }
     if (temp == 0){                                                         //if the record has gone through the above loop without "breaking"
       categoryList[j].category = dataStorage[i].category;                   //add that record's category to the list
+      catCounter++;
     }
     else temp = 0;                                                          //else, reset the temp and try again
     }
 }
+
+void removeBlanks() {
+    for(i = 0; i < 18; i++) {
+        if(Category[i] == '\0') break;
+        if(Category[i] == ' ') {
+            j = 0;
+            while(Category[i+j] != '\0') {
+                j++;
+                if(Category[i+j] == '\0'){
+                    Category[i] = '\0';
+                     break;
+                }
+                else if(Category[i+j] != ' ') break;
+            }
+        }
+    }
+    
+    for(i = 0; i < 29; i++) {
+        if(Title[i] == '\0') break;
+        if(Title[i] == ' ') {
+            j = 0;
+            while(Title[i+j] != '\0') {
+                j++;
+                if(Title[i+j] == '\0'){
+                    Title[i] = '\0';
+                     break;
+                }
+                else if(Title[i+j] != ' ') break;
+            }
+        }
+    }
+    for(i = 0; i < 140; i++) {
+        if(Body[i] == '\0') break;
+        if(Body[i] == ' ') {
+            j = 0;
+            while(Body[i+j] != '\0') {
+                j++;
+                if(Body[i+j] == '\0'){
+                    Body[i] = '\0';
+                     break;
+                }
+                else if(Body[i+j] != ' ') break;
+            }
+        }
+    }
+}
+
 int setStat(){
   readmyStoreFromChild("stat", NULL, NULL, NULL, NULL); 
   parseInputForStat();
@@ -222,45 +277,60 @@ void setup() {                    //print using myui (stat)
 }
 
 void updateRecords(int range){
-  char TempBodyA[72];
-  char TempBodyB[72];
+  free(catStorage);
+  int trow, tcol;
+  char TempBodyA[71];
+  char TempBodyB[71];
   int r;
   int i;
-  for (i = 6; i <= 24; i++){
-    xt_par2(XT_SET_ROW_COL_POS,row=i,col=20);
-    printf("                                                                      ");
+  for (i = 6; i <= 25; i++){
+    xt_par2(XT_SET_ROW_COL_POS,trow=i,tcol=20);
+    printf("                                                                       ");
  }  
+  tempCounter = 0;
+  //struct ArrayRecords *catStorage; 
+  catStorage = (struct ArrayRecords*)malloc((maxRecord) * sizeof(struct ArrayRecords));
+  for(i = 0; i < maxRecord; i++){
+    if(strcmp(dataStorage[i].category, categoryList[SUBJECT-8].category) == 0){
+      catStorage[tempCounter] = dataStorage[i];
+      tempCounter++;
+    }
+  }
   i = 0;
   for(r = range; r < 4+range; r++){
+    if (r < tempCounter){
     int k = 0;
-    while(k != 71) {
+    while(k != 70) {
       TempBodyA[k] = ' ';
       TempBodyB[k] = ' ';
       k++;
     }
-    TempBodyA[71] = '\0';
-    TempBodyB[71] = '\0';
+    TempBodyA[70] = '\0';
+    TempBodyB[70] = '\0';
     k = 0;
-    xt_par2(XT_SET_ROW_COL_POS, row = 7+5*i, col = 20);
+    xt_par2(XT_SET_ROW_COL_POS, trow = 7+5*i, tcol = 20);
     xt_par0(XT_CH_GREEN);
-    printf("Record %d (%s)", r+1, dataStorage[r].timedate); //get the time using myui1
+    printf("Record %d (%s)", r+1, catStorage[r].timedate); //get the time using myui1
     xt_par0(XT_CH_WHITE);
-    xt_par2(XT_SET_ROW_COL_POS, row = 8+5*i, col = 20);
+    xt_par2(XT_SET_ROW_COL_POS, trow = 8+5*i, tcol = 20);
     int j = 0;
-     while(dataStorage[r].body[j] != '\0'){
-      if (j < 71) TempBodyA[j] = dataStorage[r].body[j];
-      else if (j < 141) TempBodyB[j%71] = dataStorage[r].body[j];
+     while(catStorage[r].body[j] != '\0'){
+      if (j < 70) TempBodyA[j] = catStorage[r].body[j];
+      else if (j < 140) TempBodyB[j%70] = catStorage[r].body[j];
       j++;
     }
     xt_par0(XT_CH_GREEN);
-    printf("%s", dataStorage[r].subject);
+    printf("%s", catStorage[r].subject);
     xt_par0(XT_CH_WHITE);
-    xt_par2(XT_SET_ROW_COL_POS, row = 9+5*i, col = 20);
+    xt_par2(XT_SET_ROW_COL_POS, trow = 9+5*i, tcol = 20);
     printf("%s", TempBodyA);
-    xt_par2(XT_SET_ROW_COL_POS, row = 10+5*i, col = 20);
+    xt_par2(XT_SET_ROW_COL_POS, trow = 10+5*i, tcol = 20);
     printf("%s", TempBodyB);
     i++;
     }
+  }
+  xt_par2(XT_SET_ROW_COL_POS,row,col);
+  //free(catStorage);
 }
 void lifeTracker() {
   setup();
@@ -279,21 +349,18 @@ void lifeTracker() {
     printf("|");
   }
   xt_par2(XT_SET_ROW_COL_POS, row = 7, col = 2);
+  fillCategory();
   xt_par0(XT_CH_GREEN);
-  printf("---Subjects---");
+  printf("--Categories--");
   xt_par0(XT_CH_WHITE);
-  for(i = 0; i < 17; i++){
+  for(i = 0; i < 18; i++){
     xt_par2(XT_SET_ROW_COL_POS, row = 8 + i, col = 2);
-    printf("--------------");  //prints the list of subjects, utilizes the forking program/myui1  cmd(mystore, subject, i+1)
+    if (i < catCounter) printf("%s", categoryList[i].category);
+    else printf("--------------");  //prints the list of subjects, utilizes the forking program/myui1  cmd(mystore, subject, i+1)
   }
-  for(i = 0; i < 2; i++){
-    xt_par2(XT_SET_ROW_COL_POS, row = 25+i, col = 2);
-    xt_par0(XT_CH_BLUE); 
-    if (i == 0)
-      printf("---F5-Add-----");
-    else if (i == 1)
-      printf("---F6-Sort----");
-  }
+  xt_par2(XT_SET_ROW_COL_POS, row = 26, col = 2);
+  xt_par0(XT_CH_BLUE); 
+  printf("---F6-Sort----");
   updateRecords(0);
   xt_par2(XT_SET_ROW_COL_POS, row = 26, col = 20);
   xt_par0(XT_CH_BLUE);
@@ -336,17 +403,17 @@ void addScreen(){
   }
   xt_par2(XT_SET_ROW_COL_POS, row = 11, col = 25);
   printf("-----------------------------------------------------------------------");
-  xt_par2(XT_SET_ROW_COL_POS, row = 14, col = 25);
+  /*xt_par2(XT_SET_ROW_COL_POS, row = 14, col = 25);
   printf("------------------");
   xt_par2(XT_SET_ROW_COL_POS, row = 16, col = 25);
   printf("-----------------------------");
   xt_par2(XT_SET_ROW_COL_POS, row = 19, col = 25);
   printf("----------------------------------------------------------------------");
   xt_par2(XT_SET_ROW_COL_POS, row = 20, col = 25);
-  printf("----------------------------------------------------------------------");
+  printf("----------------------------------------------------------------------");*/
   xt_par0(XT_CH_GREEN);
   xt_par2(XT_SET_ROW_COL_POS, row = 12, col = 25);
-  printf("Record - (--:--:--)");                       //print the record number and time using myui1 
+  printf("Record - (---------- --:--:--)");                       //print the record number and time using myui1 
   xt_par2(XT_SET_ROW_COL_POS, row = 13, col = 25);
   printf("Category:");
   xt_par2(XT_SET_ROW_COL_POS, row = 15, col = 25);
@@ -374,16 +441,51 @@ int main() {
     while (screen == 0) {
       while ((c = getkey()) == KEY_NOTHING);
       if(c == KEY_F9) screen = 3;
-
+      else if(c == KEY_F3) {
+	char str[80];
+	for(i = 0; i < maxRecord; i++){
+	  if(strcmp(catStorage[currentRecord].subject, dataStorage[i].subject) == 0 &&
+	     strcmp(catStorage[currentRecord].body, dataStorage[i].body) == 0 &&
+	     strcmp(catStorage[currentRecord].category, dataStorage[i].category) == 0 &&
+	     strcmp(catStorage[currentRecord].timedate, dataStorage[i].timedate) == 0){           //looks for corresponding record in the actual record list
+	    sprintf(str, "%d", i+1);
+	    break;
+	  }
+	}
+        readmyStoreFromChild("delete", str, NULL, NULL, NULL);
+        setStat();
+        xt_par2(XT_SET_ROW_COL_POS, row = 1, col = 1);
+        xt_par0(XT_CH_GREEN);
+        printf("----------------------------------------------------LifeTracker---------------------------------------------------------");
+        xt_par0(XT_CH_WHITE);
+        xt_par2(XT_SET_ROW_COL_POS, row = 2, col = 35);
+        printf("Number of Records: %s |  Author: %s | Version: %s ", data[3].value, data[2].value, data[1].value);
+        xt_par2(XT_SET_ROW_COL_POS,row = 3, col = 24);
+        printf("First Record  Time: %s | Last Record  Time: %s", data[4].value, data[5].value);
+        xt_par2(XT_SET_ROW_COL_POS, row = 4, col = 1);
+        printf("------------------------------------------------------------------------------------------------------------------------");
+        xt_par2(XT_SET_ROW_COL_POS, row = 5, col = 1);
+        xt_par0(XT_CH_BLUE);
+        if (screen == 0)
+          printf("------------UP/Down-Scroll between Subjects/Records/Search Left/Right-Toggle between Subjects/Records/Search------------");
+        else 
+          printf("-------------------------------------UP/Down-Switch rows Left/Right-Switch columns--------------------------------------");
+        xt_par0(XT_CH_WHITE);
+        xt_par2(XT_SET_ROW_COL_POS, row = 7, col = 20);
+	currentRecord = recordView = 0; 
+	updateRecords(0);
+      }
       else if(c == KEY_DOWN) {
-        if(col == 2 && row >= 8 && row < 24) 
+        if(col == 2 && row >= 8 && row < catCounter + 7){ 
           xt_par2(XT_SET_ROW_COL_POS,SUBJECT = ++row,col);
-        if(col == 20 && row >= 7 && row <= 22){
+	  updateRecords(recordView);
+	}
+        if(col == 20 && row >= 7 && row <= tempCounter * 5 + 1){
           if (row < 22){
             xt_par2(XT_SET_ROW_COL_POS,row+=5,col);
             ++currentRecord;
             }
-          else if (recordView < maxRecord-4){
+          else if (recordView < tempCounter-4){ 
             ++recordView;
             updateRecords(recordView);
             xt_par2(XT_SET_ROW_COL_POS,row=22,col);
@@ -400,8 +502,10 @@ int main() {
     }
       }
       else if(c == KEY_UP){
-        if(col == 2 && row > 8 && row <= 24)
+        if(col == 2 && row > 8 && row <= 24){
           xt_par2(XT_SET_ROW_COL_POS,SUBJECT = --row,col);
+	  updateRecords(recordView);
+	}
         if(col == 20 && row >= 7 && row < 24){
           if (row <= 24 && row > 7){
             xt_par2(XT_SET_ROW_COL_POS,row-=5,col);
@@ -429,11 +533,13 @@ int main() {
     xt_par2(XT_SET_ROW_COL_POS,++row,col=95);
       else if((c == KEY_LEFT || c == KEY_RIGHT) && col == 2) {
     SUBJECT = row;
+    updateRecords(recordView);
     xt_par2(XT_SET_ROW_COL_POS,row=7,col=20);
       }
       else if((c == KEY_LEFT || c == KEY_RIGHT) && col == 20) {
         xt_par2(XT_SET_ROW_COL_POS,row=SUBJECT,col=2);
-        currentRecord = recordView;
+        currentRecord = recordView = 0;
+	updateRecords(recordView);
         }
       else if(c == KEY_LEFT && col > 95 && col < 120)
     xt_par2(XT_SET_ROW_COL_POS,row,--col);
@@ -483,17 +589,17 @@ int main() {
     }
     if (screen == 1 || screen == 2){
       int reset = 0;
-      for(reset; reset < 18; reset++) Title[reset] == ' ';
+      for(reset; reset < 18; reset++) Title[reset] = ' ';
       reset = 0;
       for(reset; reset < 140; reset++) Body[reset] = ' ';
       reset = 0;
       for(reset; reset < 29; reset++) Category[reset] = ' ';
       addScreen();
       if (screen == 2){ 
-        char TempBodyA[72]; 
-        char TempBodyB[72];
+        char TempBodyA[71]; 
+        char TempBodyB[71];
         int k = 0;
-        while(k != 71) {
+        while(k != 70) {
         TempBodyA[k] = ' ';
         TempBodyB[k] = ' ';
         k++;
@@ -501,31 +607,31 @@ int main() {
         k = 0; 
         xt_par2(XT_SET_ROW_COL_POS, row = 12, col = 25);
         xt_par0(XT_CH_GREEN);
-        printf("Record %d (%s)", currentRecord+1, dataStorage[currentRecord].timedate); //get the time using myui1
+        printf("Record %d (%s)", currentRecord+1, catStorage[currentRecord].timedate); //get the time using myui1
         xt_par0(XT_CH_WHITE);
         xt_par2(XT_SET_ROW_COL_POS, row = 14, col = 25);
-        printf("%s", dataStorage[currentRecord].category);
+        printf("%s", catStorage[currentRecord].category);
         int j = 0; 
-        while(dataStorage[currentRecord].body[j] != '\0'){
-          if (j < 71) TempBodyA[j] = dataStorage[currentRecord].body[j];
-          else if (j < 141) TempBodyB[j%71] = dataStorage[currentRecord].body[j];
+        while(catStorage[currentRecord].body[j] != '\0'){
+          if (j < 70) TempBodyA[j] = catStorage[currentRecord].body[j];
+          else if (j < 140) TempBodyB[j%70] = catStorage[currentRecord].body[j];
           j++;
         }
-        TempBodyA[71] = '\0';
-        TempBodyB[71] = '\0';
+        TempBodyA[70] = '\0';
+        TempBodyB[70] = '\0';
         //NOT RESETTING ARRAY
         //TempBodyB[70] = '\0'; 
         xt_par2(XT_SET_ROW_COL_POS, row = 16, col = 25);
-        printf("%s", dataStorage[currentRecord].subject);
+        printf("%s", catStorage[currentRecord].subject);
         xt_par0(XT_CH_WHITE);
         xt_par2(XT_SET_ROW_COL_POS, row = 19, col = 25);
         printf("%s", TempBodyA); 
         xt_par2(XT_SET_ROW_COL_POS, row = 20, col = 25); 
         printf("%s", TempBodyB); 
         xt_par2(XT_SET_ROW_COL_POS, row = 14, col = 25); 
-        strncpy(Title, dataStorage[currentRecord].subject, sizeof(Title));
-        strncpy(Body, dataStorage[currentRecord].body, sizeof(Body));
-        strncpy(Category, dataStorage[currentRecord].category, sizeof(Category));
+        strncpy(Title, catStorage[currentRecord].subject, sizeof(Title));
+        strncpy(Body, catStorage[currentRecord].body, sizeof(Body));
+        strncpy(Category, catStorage[currentRecord].category, sizeof(Category));
         //print corresponding record in correct locations using myui1 
       }
     } 
@@ -569,27 +675,59 @@ int main() {
       else if (c == KEY_ENTER && row > 18 && row < 20) 
         xt_par2(XT_SET_ROW_COL_POS,++row,col=25);
         
-      else if (c == KEY_BACKSPACE && col > 25 && col < 95 && ((row > 18 && row < 21) || row == 16 || row == 14)) {
-        xt_par2(XT_SET_ROW_COL_POS,row,--col);
-        putchar(' ');
-        xt_par2(XT_SET_ROW_COL_POS,row,col);
-      } 
-      else if (c == KEY_BACKSPACE && col == 25 && row == 20){
-        xt_par2(XT_SET_ROW_COL_POS,--row,col=94);
-        putchar(' ');
-        xt_par2(XT_SET_ROW_COL_POS,row,col);
+      else if (c == KEY_BACKSPACE && col >= 25 && col < 95 && ((row > 18 && row < 21) || row == 16 || row == 14)){
+	if(row == 14 && col > 25 && col <= 43) {
+	  xt_par2(XT_SET_ROW_COL_POS,row,--col);
+	  putchar(' ');
+	  Category[col - 25] = ' ';
+	}
+	else if(row == 16 && col > 25 && col <= 54) {
+	  xt_par2(XT_SET_ROW_COL_POS,row,--col);
+	  putchar(' ');
+	  Title[col - 25] = ' ';
+	}
+	else if(row == 19 && col > 25 && col <= 95) {
+	  xt_par2(XT_SET_ROW_COL_POS,row,--col);
+	  putchar(' ');
+	  Body[col - 25] = ' ';
+	}
+	else if(row == 20 && col > 25 && col <= 95) {
+	  xt_par2(XT_SET_ROW_COL_POS,row,--col);
+	  putchar(' ');
+	  Body[col + 45] = ' ';
+	}
+	//if (col < 94 && !(row == 16 && col > 53) && !(row == 14 && col > 42)){
+	//  --col; 
+	//}
+	else if(row == 20){ 
+	  //if (row == 20)                                            
+	    xt_par2(XT_SET_ROW_COL_POS,--row,col=94);
+	    putchar(' ');
+	    Body[69] = ' ';
+	  // else xt_par2(XT_SET_ROW_COL_POS,row,col); 
+	}
+	xt_par2(XT_SET_ROW_COL_POS,row,col);
+	
       }
       else if (c == KEY_DELETE) {
-        putchar(' ');
+        if(row == 14 && col >= 25 && col < 43) {
+              putchar(' ');
+              Category[col - 26] = ' ';
+            }
+            else if(row == 16 && col >= 25 && col < 54) {
+              putchar(' ');
+              Title[col - 26] = ' ';
+            }
+            else if(row == 19 && col >= 25 && col <= 95) {
+              putchar(' ');
+              Body[col - 26] = ' ';
+            }
+            else if(row == 20 && col >= 25 && col <= 95) {
+              putchar(' ');
+              Body[col + 46] = ' ';
+            }
         xt_par2(XT_SET_ROW_COL_POS,row,col);
       } 
-      else if(c == KEY_F3) {
-        char str[80];
-        sprintf(str, "%d", currentRecord);
-        readmyStoreFromChild("delete", str, NULL, NULL, NULL);
-        readmyStoreFromChild("add", "TESTING", "DUNNO", "WORKS?", NULL);
-        setup();
-      }
       //else if(screen == 1 || screen == 2) {
         else if((c >= ' ' && c <= '~') && col >= 25 && col < 95) {
             if(row == 14 && col >= 25 && col < 43) {
@@ -620,6 +758,7 @@ int main() {
       }
       else if(c == KEY_F2 && screen == 1 /* && there is a valid title and description */){
     //save record to corresponding subject
+	removeBlanks();
         readmyStoreFromChild("add", Title, Body, Category, NULL);
         int k = 0;
         while(k != 18) Title[k++] = ' ';
@@ -627,13 +766,22 @@ int main() {
         while(k != 140) Body[k++] = ' ';
         k = 0;
         while(k != 29) Category[k++] = ' ';
-	maxRecord++;
+	      maxRecord++;
         screen = 0;
       }
       else if(c == KEY_F2 && screen == 2 /* && there has been a valid change in the record */){
         //update record
+	removeBlanks();
         char str[80];
-        sprintf(str, "%d", currentRecord+1);
+	for(i = 0; i < maxRecord; i++){
+	  if(strcmp(catStorage[currentRecord].subject, dataStorage[i].subject) == 0 &&
+	     strcmp(catStorage[currentRecord].body, dataStorage[i].body) == 0 &&
+	     strcmp(catStorage[currentRecord].category, dataStorage[i].category) == 0 &&
+	     strcmp(catStorage[currentRecord].timedate, dataStorage[i].timedate) == 0){           //looks for corresponding record in the actual record list
+	    sprintf(str, "%d", i+1);
+	    break;
+	  }
+	}
         readmyStoreFromChild("edit", str, Title, Body, Category);
         int k = 0;
         while(k != 18) Title[k++] = ' ';
